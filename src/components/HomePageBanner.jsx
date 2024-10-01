85fde74d7e24fb0d2683baa2bb2dbf19dd52962d
fernandosalomon <fernandosalomon1992@gmail.com>
2024-09-30 22:01:46 -0300

    create HomePage

--- src/components/HomePageBanner.jsx
+++ src/components/HomePageBanner.jsx
@@ -1,13 +1,16 @@
 import "../styles/HomePageBanner.css";
+import ButtonC from "./shared/ButtonC";
 
 const HomePageBanner = () => {
   return (
-    <div className="container">
-      <h2 className="main-title">Rolling Paws</h2>
-      <h3>Siempre al lado de tu mascota</h3>
-      <div>
-        <button>Sobre Nosotros</button>
-        <button>Reservar Cita</button>
+    <div className="main-wrapper">
+      <div className="banner-wrapper">
+        <h2 className="main-title mb-3">Rolling Paws</h2>
+        <h3 className="subtitle mb-5">Siempre al lado de tu mascota</h3>
+        <div>
+          <ButtonC>Sobre Nosotros</ButtonC>
+          <ButtonC>Reservar Cita</ButtonC>
+        </div>
       </div>
     </div>
   );
