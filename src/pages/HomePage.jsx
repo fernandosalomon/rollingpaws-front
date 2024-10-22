d0f0714d3ea7b012a9d7be733b30844a4b45c682
fernandosalomon <fernandosalomon1992@gmail.com>
2024-10-22 09:48:51 -0300

    modify landing page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,11 @@
-import HomePageBanner from "../components/HomePageBanner";
-import HomePageIntro from "../components/HomePageIntro";
+import LandingPage from "../components/LandingPage";
 import Services from "../components/Services";
-import Testimonials from "../components/Testimonials";
-import { Slideshow, Slide } from "../components/shared/Slideshow";
-import "../styles/HomePage.css";
 
 const HomePage = () => {
   return (
     <>
-      <div className="page-bg"></div>
-      <HomePageBanner />
-      <HomePageIntro />
-      <Testimonials />
-      <Services />
+      <LandingPage />
+      
     </>
   );
 };
