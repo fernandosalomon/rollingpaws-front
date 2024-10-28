72ae8043834286fc0aec9aad99fc79173e343f51
fernandosalomon <fernandosalomon1992@gmail.com>
2024-10-27 22:37:37 -0300

    fix style ServicePage

--- src/components/ServicesPage.jsx
+++ src/components/ServicesPage.jsx
@@ -1,28 +1,42 @@
-import ListGroup from "react-bootstrap/ListGroup";
 import Image from "react-bootstrap/Image";
-import style from "../styles/Services.module.css";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
+import style from "../styles/Services.module.css";
 import { useState } from "react";
 import ButtonC from "./shared/ButtonC";
+import {
+  CardBody,
+  CardFooter,
+  CardHeader,
+  CardImage,
+  CardText,
+  CustomCard,
+} from "./shared/CustomCard";
 
 const ClinicaGeneral = () => {
   return (
     <>
-      <div className={`${style.imageContainer}`}>
-        <Image src="./src/assets/img/services/clinica-general-service.jpg" />
-      </div>
-      <h2 className={style.clinicaGeneralHeader}>
-        Clínica General y Especialidades
-      </h2>
-      <p className={style.servicesDescriptionText}>
-        Ofrecemos servicios de clínica general y especializada para cuidar la
-        salud de tu mascota en todo momento, desde consultas de rutina hasta
-        diagnósticos y tratamientos avanzados.
-      </p>
-
-      <div className="d-flex justify-content-center align-items-center w-100 my-4">
-        <ButtonC variant={"button1"}>Saber más</ButtonC>
-      </div>
+      <CustomCard height={"100%"} width={"100%"} className="flex-fill">
+        <CardImage
+          src={"src/assets/img/services/clinica-general-service.jpg"}
+          alt={"Clínica General y Especialidades"}
+          className={"d-flex justify-content-center align-items-center"}
+        />
+        <CardBody>
+          <CardHeader className={style.title}>
+            Clínica General y Especialidades
+          </CardHeader>
+          <CardText className={style.descriptionText}>
+            Ofrecemos servicios de clínica general y especializada para cuidar
+            la salud de tu mascota en todo momento, desde consultas de rutina
+            hasta diagnósticos y tratamientos avanzados.
+          </CardText>
+        </CardBody>
+        <CardFooter>
+          <ButtonC variant={"button1"}>Saber más</ButtonC>
+        </CardFooter>
+      </CustomCard>
     </>
   );
 };
@@ -30,48 +44,51 @@ const ClinicaGeneral = () => {
 const Cirugia = () => {
   return (
     <>
-      <div className="d-flex h-100">
-        <div className="w-50 d-flex flex-column justify-content-center gap-2">
-          <h2 className={style.cirugiaHeader}>Cirugía</h2>
-          <p className={style.cirugiaText}>
+      <CustomCard height={"100%"} width={"100%"} className="flex-fill">
+        <CardImage
+          src={"./src/assets/img/services/dog-surgery.jpg"}
+          alt={"Cirugía"}
+          className={"d-flex justify-content-center align-items-center"}
+        />
+        <CardBody>
+          <CardHeader className={style.title}>Cirugía</CardHeader>
+          <CardText className={style.descriptionText}>
             Nuestro servicio de cirugía veterinaria garantiza procedimientos
             seguros y precisos, con un equipo altamente capacitado y tecnología
             moderna. Nos enfocamos en una recuperación rápida y en el bienestar
             integral de tu mascota.
-          </p>
-          <div className="d-flex justify-content-center align-items-center">
-            <ButtonC variant={"button1"}>Saber más</ButtonC>
-          </div>
-        </div>
-        <div className={`${style.imageContainer} w-50 h-100`}>
-          <Image
-            src="./src/assets/img/services/dog-surgery.jpg"
-            className="h-100"
-          />
-        </div>
-      </div>
+          </CardText>
+        </CardBody>
+        <CardFooter>
+          <ButtonC variant={"button1"}>Saber más</ButtonC>
+        </CardFooter>
+      </CustomCard>
     </>
   );
 };
 
-const CorteDePelo = () => {
+const Peluqueria = () => {
   return (
     <>
-      <div className={style.clinicaGeneralImagePeluqueria}>
-        <Image src="./src/assets/img/services/dog-haircut.jpg" />
-      </div>
-      <h2 className={`${style.clinicaGeneralHeader} text-center`}>
-        Peluquería
-      </h2>
-      <p className={`${style.clinicaGeneralText} text-center`}>
-        Ofrecemos un cuidado integral que incluye baño, corte de pelo, limpieza
-        de oídos y corte de uñas, todo adaptado a las necesidades de tu mascota.
-        Nos enfocamos en su higiene, confort y apariencia, brindándole una
-        experiencia completa
-      </p>
-      <div className="d-flex justify-content-center align-items-center w-100 my-4">
-        <ButtonC variant={"button1"}>Saber más</ButtonC>
-      </div>
+      <CustomCard height={"100%"} width={"100%"} className="flex-fill">
+        <CardImage
+          src={"src/assets/img/services/dog-haircut.jpg"}
+          alt={"Peluquería"}
+          className={"d-flex justify-content-center align-items-center"}
+        />
+        <CardBody>
+          <CardHeader className={style.title}>Peluquería</CardHeader>
+          <CardText className={style.descriptionText}>
+            Ofrecemos un cuidado integral que incluye baño, corte de pelo,
+            limpieza de oídos y corte de uñas, todo adaptado a las necesidades
+            de tu mascota. Nos enfocamos en su higiene, confort y apariencia,
+            brindándole una experiencia completa.
+          </CardText>
+        </CardBody>
+        <CardFooter>
+          <ButtonC variant={"button1"}>Saber más</ButtonC>
+        </CardFooter>
+      </CustomCard>
     </>
   );
 };
@@ -110,21 +127,25 @@ const PlanesDeSalud = () => {
 const Tienda = () => {
   return (
     <>
-      <div className={style.imageContainer}>
-        <Image src="./src/assets/img/services/store.jpg" />
-      </div>
-      <h2 className={`${style.clinicaGeneralHeader} text-center`}>
-        Tienda de Productos
-      </h2>
-      <p className={style.servicesDescriptionText}>
-        Nuestra tienda ofrece una selección de productos de alta calidad para el
-        cuidado y bienestar de tu mascota, desde alimentos especializados hasta
-        accesorios y juguetes. Todo lo que necesitas para mantener a tu
-        compañero sano y feliz, en un solo lugar.
-      </p>
-      <div className="d-flex justify-content-center align-items-center w-100 my-4">
-        <ButtonC variant={"button1"}>Ir a la tienda</ButtonC>
-      </div>
+      <CustomCard height={"100%"} width={"100%"} className="flex-fill">
+        <CardImage
+          src={"src/assets/img/services/store.jpg"}
+          alt={"Tienda de Productos"}
+          className={"d-flex justify-content-center align-items-center"}
+        />
+        <CardBody>
+          <CardHeader className={style.title}>Tienda de Productos</CardHeader>
+          <CardText className={style.descriptionText}>
+            Nuestra tienda ofrece una selección de productos de alta calidad
+            para el cuidado y bienestar de tu mascota, desde alimentos
+            especializados hasta accesorios y juguetes. Todo lo que necesitas
+            para mantener a tu compañero sano y feliz, en un solo lugar.
+          </CardText>
+        </CardBody>
+        <CardFooter>
+          <ButtonC variant={"button1"}>Ir a la tienda</ButtonC>
+        </CardFooter>
+      </CustomCard>
     </>
   );
 };
@@ -135,8 +156,89 @@ const ServicesPage = () => {
   return (
     <>
       <Container fluid className={style.wrapper}>
-        <div className={style.servicesListWrapper}>
-          <ListGroup as="ul" className={style.servicesList}>
+        <Row className="w-100">
+          <Col xs={12} md={3} className={style.leftPanelList}>
+            <Row className={style.servicesList} style={{ height: "80vh" }}>
+              <Col>
+                <button
+                  className={`${style.buttonLeftPanel} ${
+                    serviceActive === 0 && style.active
+                  }`}
+                  onClick={() => {
+                    setServiceActive(0);
+                  }}
+                >
+                  Clínica general y especialidades
+                </button>
+              </Col>
+              <Col>
+                <button
+                  className={`${style.buttonLeftPanel} ${
+                    serviceActive === 1 && style.active
+                  }`}
+                  onClick={() => {
+                    setServiceActive(1);
+                  }}
+                >
+                  Cirugía
+                </button>
+              </Col>
+              <Col>
+                <button
+                  className={`${style.buttonLeftPanel} ${
+                    serviceActive === 2 && style.active
+                  }`}
+                  onClick={() => {
+                    setServiceActive(2);
+                  }}
+                >
+                  Peluquería
+                </button>
+              </Col>
+              <Col>
+                <button
+                  className={`${style.buttonLeftPanel} ${
+                    serviceActive === 3 && style.active
+                  }`}
+                  onClick={() => {
+                    setServiceActive(3);
+                  }}
+                >
+                  Planes de salud
+                </button>
+              </Col>
+              <Col>
+                <button
+                  className={`${style.buttonLeftPanel} ${
+                    serviceActive === 4 && style.active
+                  }`}
+                  onClick={() => {
+                    setServiceActive(4);
+                  }}
+                >
+                  Tienda de productos
+                </button>
+              </Col>
+            </Row>
+          </Col>
+          <Col xs={12} md={9}>
+            <div className="w-100 py-3" style={{ height: "80vh" }}>
+              {serviceActive === 0 && <ClinicaGeneral />}
+              {serviceActive === 1 && <Cirugia />}
+              {serviceActive === 2 && <Peluqueria />}
+              {serviceActive === 3 && <PlanesDeSalud />}
+              {serviceActive === 4 && <Tienda />}
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default ServicesPage;
+
+/*<ListGroup as="ul" className={style.servicesList}>
             <ListGroup.Item
               as="li"
               className={`${style.servicesListItem} ${
@@ -182,18 +284,4 @@ const ServicesPage = () => {
             >
               Tienda
             </ListGroup.Item>
-          </ListGroup>
-        </div>
-        <div className={style.servicesDescription}>
-          {serviceActive === 0 && <ClinicaGeneral />}
-          {serviceActive === 1 && <Cirugia />}
-          {serviceActive === 2 && <CorteDePelo />}
-          {serviceActive === 3 && <PlanesDeSalud />}
-          {serviceActive === 4 && <Tienda />}
-        </div>
-      </Container>
-    </>
-  );
-};
-
-export default ServicesPage;
+          </ListGroup>*/
