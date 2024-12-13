96179d5e6cfbe22eb8e129dbe621a39e9fb5ac2f
fernandosalomon <fernandosalomon1992@gmail.com>
2024-12-13 13:57:49 -0300

    fetch data of appointments and style

--- src/pages/Appointments.jsx
+++ src/pages/Appointments.jsx
@@ -1,6 +1,10 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import CustomCalendar from "../components/CustomCalendar";
 import style from "../styles/AppointmentAdmin.module.css";
+import clientAxios from "../helpers/clientAxios";
+import { Spinner, ToastContainer } from "react-bootstrap";
+import Toast from "react-bootstrap/Toast";
+import Table from "react-bootstrap/Table";
 
 const Appointments = () => {
   const today = new Date();
@@ -12,6 +16,15 @@ const Appointments = () => {
   const [currentDate, setCurrentDate] = useState(new Date().getDate());
   const [currentMonth, setCurrentMonth] = useState(new Date().getMonth());
   const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
+  const [appointments, setAppointments] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [showToast, setShowToast] = useState(false);
+  const [position, setPosition] = useState({});
+  const [toastData, setToastData] = useState([]);
+  const toastRef = useRef(null);
+  const containerRef = useRef(null);
+
   const dayDictionary = {
     0: "Dom",
     1: "Lun",
@@ -101,12 +114,7 @@ const Appointments = () => {
       selectedDate.getDate() + i
     );
 
-    week.push({
-      date: nextDay.getDate(),
-      day: dayDictionary[nextDay.getDay()],
-      fullYear: nextDay.getFullYear(),
-      month: nextDay.getMonth(),
-    });
+    week.push(nextDay);
   }
 
   const handlePrevDay = () => {
@@ -138,93 +146,262 @@ const Appointments = () => {
     }
   };
 
+  const positionAppointment = (date, startDate, endDate) => {
+    if (date >= startDate && date <= endDate) {
+      const dayIndex = date.getDate() - startDate.getDate();
+      const hour = date.getHours();
+      return {
+        top: `${64 + (hour - openingHour) * 128}px`,
+        left: `${100 + dayIndex * 150}px`,
+      };
+    }
+  };
+
+  const handleClickAppointment = ({ top, left }, data) => {
+    if (showToast) {
+      setShowToast(false);
+    } else {
+      setPosition({
+        top: `calc(${top} - 36px)`,
+        left: `calc(${left} + 160px)`,
+      });
+      setToastData(data);
+      setShowToast(true);
+    }
+  };
+
   useEffect(() => {
     setSelectedDate(new Date(currentYear, currentMonth, currentDate));
   }, [currentYear, currentMonth, currentDate]);
 
-  return (
-    <div>
-      <div className={style.container}>
-        <div className={style.sideA}>
-          <CustomCalendar
-            border
-            handleSetDate={handleSetDate}
-            pickedDate={selectedDate}
-            allowPreviousDates={true}
-          />
-        </div>
-        <div className={style.sideB}>
-          <div className={style.sideBHeader}>
-            <h4 className="mb-0">{`${
-              monthInYear[selectedDate.getMonth()].monthName
-            } ${selectedDate.getFullYear()}`}</h4>
-            <button
-              className={`${style.button} ${style.todayButton}`}
-              onClick={(e) => {
-                e.preventDefault();
-                setSelectedDate(today);
-              }}
-            >
-              Hoy
-            </button>
-            <div className={style.controlButtons}>
-              <button
-                className={style.button}
-                onClick={(e) => {
-                  e.preventDefault();
-                  handlePrevDay();
-                }}
-              >
-                &lt;
-              </button>
-              <button
-                className={style.button}
-                onClick={(e) => {
-                  e.preventDefault();
-                  handleNextDay();
-                }}
-              >
-                &gt;
-              </button>
-            </div>
+  useEffect(() => {
+    const fetchAppointments = async () => {
+      try {
+        setIsLoading(true);
+        const res = await clientAxios.get("/appointments/");
+        setAppointments(res.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchAppointments();
+  }, []);
+
+  useEffect(() => {
+    if (showToast && toastRef.current && containerRef.current) {
+      const { top, left, width, height } =
+        toastRef.current.getBoundingClientRect();
+      const containerRect = containerRef.current.getBoundingClientRect();
+      const scrollTop = containerRef.current.scrollTop;
+      const scrollLeft = containerRef.current.scrollLeft;
+      containerRef.current.scrollTo({
+        top: scrollTop + top - containerRect.height / 2 + height / 2,
+        left: scrollLeft + left - containerRect.width / 2 + width / 2,
+        behavior: "smooth",
+      });
+    }
+  }, [showToast]);
+
+  if (isLoading) {
+    return (
+      <Spinner animation="border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+    );
+  } else {
+    return (
+      <div>
+        <div className={style.container}>
+          <div className={style.sideA}>
+            <CustomCalendar
+              border
+              handleSetDate={handleSetDate}
+              pickedDate={selectedDate}
+              allowPreviousDates={true}
+            />
           </div>
-          <div className={style.sideBBody}>
-            <table className={style.table}>
-              <thead className={style.tableHeader}>
-                <tr>
-                  <th className={style.hourCell}></th>
-                  {week.map((weekDay, index) => (
-                    <th key={index}>{`${weekDay.day} ${weekDay.date}`}</th>
-                  ))}
-                </tr>
-              </thead>
-              <tbody className={style.tableBody}>
-                {hours.map((hour, index) => (
+          <div className={style.sideB} ref={containerRef}>
+            <div className={style.sideBHeader}>
+              <h4 className="mb-0">{`${
+                monthInYear[selectedDate.getMonth()].monthName
+              } ${selectedDate.getFullYear()}`}</h4>
+              <button
+                className={`${style.button} ${style.todayButton}`}
+                onClick={(e) => {
+                  e.preventDefault();
+                  setSelectedDate(today);
+                }}
+              >
+                Hoy
+              </button>
+              <div className={style.controlButtons}>
+                <button
+                  className={style.button}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    handlePrevDay();
+                  }}
+                >
+                  &lt;
+                </button>
+                <button
+                  className={style.button}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    handleNextDay();
+                  }}
+                >
+                  &gt;
+                </button>
+              </div>
+            </div>
+            <div className={style.sideBBody}>
+              <table className={style.table}>
+                <thead className={style.tableHeader}>
                   <tr>
-                    <td className={style.hourCell}>{hour}</td>
-                    {week.map((weekDay) => (
-                      <td
-                        onClick={() => {
-                          console.log(
-                            new Date(
-                              weekDay.fullYear,
-                              weekDay.month,
-                              weekDay.date,
-                              openingHour + index
-                            )
-                          );
-                        }}
-                      ></td>
+                    <th className={style.hourCell}></th>
+                    {week.map((weekDay, index) => (
+                      <th key={index}>{`${
+                        dayDictionary[weekDay.getDay()]
+                      } ${weekDay.getDate()}`}</th>
                     ))}
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody className={style.tableBody}>
+                  {hours.map((hour, index) => (
+                    <tr>
+                      <td className={style.hourCell}>{hour}</td>
+                      {week.map((weekDay) => (
+                        <td></td>
+                      ))}
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+              {appointments.map((appointment) =>
+                new Date(appointment.date) >= week[0] &&
+                new Date(appointment.date) <= week[week.length - 1] ? (
+                  <>
+                    <div
+                      style={positionAppointment(
+                        new Date(appointment.date),
+                        week[0],
+                        week[week.length - 1]
+                      )}
+                      className={style.appointmentBoxContainer}
+                      onClick={() => {
+                        handleClickAppointment(
+                          positionAppointment(
+                            new Date(appointment.date),
+                            week[0],
+                            week[week.length - 1]
+                          ),
+                          appointment
+                        );
+                      }}
+                    >
+                      <div className={style.appointmentBox}>
+                        <p className={style.appointmentBoxPacient}>
+                          {appointment.pet.name}
+                        </p>
+                        <p className={style.appointmentBoxTime}>{`${new Date(
+                          appointment.date
+                        ).getHours()}:00 - ${
+                          new Date(appointment.date).getHours() + 1
+                        }:00`}</p>
+                      </div>
+                    </div>
+                    {showToast && (
+                      <ToastContainer
+                        style={{
+                          position: "absolute",
+                          top: position.top,
+                          left: position.left,
+                        }}
+                        className={style.toastContainer}
+                        ref={toastRef}
+                      >
+                        <Toast onClose={() => setShowToast(false)}>
+                          <Toast.Header>
+                            <strong className="me-auto">Cita Programada</strong>
+                          </Toast.Header>
+                          <Toast.Body>
+                            <Table>
+                              <tr className={style.menuBodyRow}>
+                                <td className={style.menuBodyCellLabel}>
+                                  Paciente
+                                </td>
+                                <td>{toastData.pet.name}</td>
+                              </tr>
+                              <tr className={style.menuBodyRow}>
+                                <td className={style.menuBodyCellLabel}>
+                                  Veterinario
+                                </td>
+                                <td>{toastData.doctor}</td>
+                              </tr>
+
+                              <tr className={style.menuBodyRow}>
+                                <td className={style.menuBodyCellLabel}>
+                                  Horario
+                                </td>
+                                <td>{`${new Date(
+                                  toastData.date
+                                ).getDate()}/${new Date(
+                                  toastData.date
+                                ).getMonth()}/${new Date(
+                                  toastData.date
+                                ).getFullYear()} ${new Date(
+                                  toastData.date
+                                ).getHours()}:${new Date(
+                                  toastData.date
+                                ).getMinutes()}`}</td>
+                              </tr>
+
+                              <tr className={`${style.menuBodyRow} border-0`}>
+                                <td className={style.menuBodyCellLabel}>
+                                  Observaciones:
+                                </td>
+                              </tr>
+                              <tr>
+                                <td>{toastData.observations}</td>
+                              </tr>
+                            </Table>
+                            <div className="d-flex justify-content-end">
+                              <button className={style.editButton}>
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  width="16"
+                                  height="16"
+                                  fill="currentColor"
+                                  class="bi bi-pencil-square"
+                                  viewBox="0 0 16 16"
+                                >
+                                  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                  <path
+                                    fill-rule="evenodd"
+                                    d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"
+                                  />
+                                </svg>
+                                <p className="mb-0">Editar</p>
+                              </button>
+                            </div>
+                          </Toast.Body>
+                        </Toast>
+                      </ToastContainer>
+                    )}
+                  </>
+                ) : (
+                  ""
+                )
+              )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default Appointments;
