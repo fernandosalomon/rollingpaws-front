f7af02bcbdfba3a01e1b953e9d77ada1e05cf24b
fernandosalomon <fernandosalomon1992@gmail.com>
2024-12-21 22:39:50 -0300

    style PlansPage

--- src/routes/PageViews.jsx
+++ src/routes/PageViews.jsx
@@ -1,6 +1,5 @@
 import { Routes, Route, useLocation } from "react-router-dom";
 import { AnimatePresence, motion } from "framer-motion";
-import HomePage from "../pages/HomePage";
 import NavbarC from "../components/NavbarC";
 import AboutUsPage from "../components/AboutUsPage";
 import LandingPage from "../components/LandingPage";
@@ -10,7 +9,6 @@ import ContactUsPage from "../components/ContactUsPage";
 import style from "../styles/PageView.module.css"
 import DoctorsPage from "../components/DoctorsPage";
 import AboutPlans from "../components/AboutPlans";
-import PlansBanner from "../components/PlansBanner";
 
 const PageWrapper = ({ children }) => {
   return <motion.div initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: -20 }} transition={{ duration: 0.3 }} className={style.pageWrapper}>{children}</motion.div>
