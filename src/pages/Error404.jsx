39d1d838b943c945e230d0c5d4d3f688601bf212
fernandosalomon <fernandosalomon1992@gmail.com>
2024-12-22 18:35:55 -0300

    create Error 404 page

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,5 +1,19 @@
+import Image from "react-bootstrap/Image"
+import style from "../styles/Error404.module.css"
+import CustomButton from "../components/shared/CustomButton";
+import { useNavigate } from "react-router-dom";
+
 const Error404 = () => {
-  return <div>Error404</div>;
+
+  const navigate = useNavigate();
+
+  return <div className={style.error404Wrapper}>
+    <Image src="src/assets/img/error404_img.jpg" alt="Error 404 Image" width={"300px"} />
+    <h2 className={style.error404Title}>Woof Woof! Woof Woof!</h2>
+    <h3 className={style.error404Text}>Parece que a esta página se la comió el perro y no podemos presentarla</h3>
+    <CustomButton variant="callToAction" onClick={() => navigate(-1)}>Regresame a donde estaba</CustomButton>
+
+  </div>;
 };
 
 export default Error404;
