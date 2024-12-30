a6ff24d7099117f80bc8ae16b592d362ca16a6ac
fernandosalomon <fernandosalomon1992@gmail.com>
2024-12-30 11:24:13 -0300

    fix route to vercel deployed back

--- src/helpers/clientAxios.js
+++ src/helpers/clientAxios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const DEV_BASE_URL = `http://localhost:3001/api`;
-const VERCEL_BASE_URL = "rollingpaws-back.vercel.app/api"
+const VERCEL_BASE_URL = "http://rollingpaws-back.vercel.app/api"
 
 const clientAxios = axios.create({
   baseURL: VERCEL_BASE_URL,
