abaef8cbfff32a3b718c20212d8cca67a472aa10
fernandosalomon <fernandosalomon1992@gmail.com>
2024-12-30 11:21:53 -0300

    add vercel deployed backend

--- src/helpers/clientAxios.js
+++ src/helpers/clientAxios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const DEV_BASE_URL = `http://localhost:3001/api`;
+const VERCEL_BASE_URL = "rollingpaws-back.vercel.app/api"
+
 const clientAxios = axios.create({
-  baseURL: `http://localhost:3001/api`,
+  baseURL: VERCEL_BASE_URL,
 });
 
 export default clientAxios;
