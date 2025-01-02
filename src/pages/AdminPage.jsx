56c52d13f188caff409b25efdcdfc47cbdb29eda
fernandosalomon <fernandosalomon1992@gmail.com>
2025-01-02 14:25:14 -0300

    change 'citas' to 'turnos' in admin panel

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -55,7 +55,7 @@ const AdminPage = () => {
                   <path d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5z" />
                   <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z" />
                 </svg>
-                <p className="mb-0">Citas</p>
+                <p className="mb-0">Turnos</p>
               </li>
               <li className={`${style.sideMenuItem} ${view === 3 ? style.selected : ""}`} onClick={() => navigate("/admin/mensajes")}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chat-fill" viewBox="0 0 16 16">
