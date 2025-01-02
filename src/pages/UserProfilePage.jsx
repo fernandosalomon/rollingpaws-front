c01f51ba97bdbdbf4c0566029dbd204e4aaecae4
fernandosalomon <fernandosalomon1992@gmail.com>
2025-01-01 23:18:50 -0300

    add new appointment button to UserProfilePage

--- src/pages/UserProfilePage.jsx
+++ src/pages/UserProfilePage.jsx
@@ -172,6 +172,26 @@ const UserAppointments = () => {
   } else {
     return (
       <>
+        <div className="d-flex align-items-center justify-content-center w-100">
+          <CustomButton
+            className="d-flex align-items-center ms-md-auto ms-0 mt-3"
+            onClick={handleShowNewAppointment}
+            variant="transparent"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="currentColor"
+              className="bi bi-calendar-event me-2"
+              viewBox="0 0 16 16"
+            >
+              <path d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5z" />
+              <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z" />
+            </svg>
+            <p className="m-0">Pedir Turno</p>
+          </CustomButton>
+        </div>
         <div className={style.viewHeader}>
           <h2 className={style.viewHeaderLabel}>Mis Turnos</h2>
         </div>
